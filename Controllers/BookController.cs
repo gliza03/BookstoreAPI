c7f5cb84c3b8a4406bd830628e320e9d3c2cc16b
gabe <gabrielliza012@gmail.com>
2025-03-21 19:49:48 -0600

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11.API.Data;
+
+namespace Mission11.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _context;
+        public BookController(BookDbContext context) => _context = context;
+        public IEnumerable<Book> Get() => _context.Books.ToList();
+    }
+}
---
+++ Data/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace Mission11.API.Data
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Publisher { get; set; }
+        public string ISBN { get; set; }
+        public string Classification { get; set; }
+        public string Category { get; set; }
+        public int PageCount { get; set; }
+        public float Price { get; set; }
+
+    }
+}
